ba65aa8dadac4ede030b642c468a3c8799657077
Monniasza <monniasza@onet.pl>
2023-09-26 14:05:57 +0200

    move form elements outside form

--- lotnisko.php
+++ lotnisko.php
@@ -80,74 +80,82 @@
             </section>
         </section>
         <section id="main">
-            <form name="edit" method="POST" action="operation.php">
-                <table>
-                    <tr>
-                        <th>ID <?php
-                            global $decl;
-                            $decl = substr($table, 0, strlen($table)-1)."u";
-                            echo $decl;
-                        ?></th>
-                        <th>czas</th>
-                        <th>data</th>
-                        <th>kierunek</th>
-                        <th>numer rejsu</th>
-                        <th>numer samolotu</th>
-                        <th>status</th>
-                    </tr>
-                    <?php
-                        $conn = mysqli_connect("localhost", "root", null, "egzamin");
-                        $query = "SELECT czas, kierunek, nr_rejsu, status_lotu, id, samoloty_id, dzien FROM ".$table." WHERE czas >= ? AND czas <= ? ORDER BY czas ASC";
-                        $stmt = $conn -> prepare($query);
-                        $stmt ->bind_param("ss", $from, $to);
-                        $stmt -> execute();
-                        $result = $stmt -> get_result();
-                        while($row = mysqli_fetch_row($result)){
-                            $czas = $row[0];
-                            $kierunak = $row[1];
-                            $nr = $row[2];
-                            $status = $row[3];
-                            $id = $row[4];
-                            $samolot = $row[5];
-                            $day = $row[6];
-                            echo "<tr><td>", $id, "</td><td>", $czas, "</td><td>", $day, "</td><td>", $kierunak, "</td><td>", $nr, "</td><td>", $samolot, "</td><td>", $status, "</td></tr>";
-                        }
-                        mysqli_close($conn);
-                    ?>
-                    <!-- Formularz dodawania/edycji (w tej samej tabeli) -->
-                    <tr> <td colspan=7>
-                            <?php echo '<input name="tab" type="hidden" value="'.$table.'"/>'?>
-                            <input name="op" type="hidden" value="insert"/>
-                            Wpisz ID <?php echo $decl; ?> do edycji, lub 0 do dodania nowego przylotu
-                            <input type="submit" value="Prześlij"/>
-                    </td> </tr>
-                    <tr>
-                        <th>ID lub wstaw</th>
-                        <th>Czas</th>
-                        <th>Data</th>
-                        <th>kierunek</th>
-                        <th>Numer rejsu</th>
-                        <th>Numer samolotu</th>
-                        <th>Status</th>
-                    </tr>
-                    <tr>
-                        <td><input type="number" name="id" value=0/></td>
-                        <td><input type="time" name="czas" value=0/></td>
-                        <td><input type="date" name="dzien" value=0/></td>
-                        <td><input type="text" name="kierunek" value=0/></td>
-                        <td><input type="text" name="nr_rejsu" value=0/></td>
-                        <td><input type="number" name="samoloty_id" value=0/></td>
-                        <td><input type="text" name="status_lotu" value="0"/></td>
-                    </tr>
-                </table>
-            </form>
-            <!-- Formularz usuwania -->
-            <form name="del" method="POST" action="operation.php">
-            <?php echo '<input name="tab" type="hidden" value="'.$table.'"/>'?>
+            <!-- Formularz usuwania - nagłówek -->
+            <form name="del" id="del" method="POST" action="operation.php">
+                <?php echo '<input name="tab" type="hidden" value="'.$table.'"/>'?>
                 <input name="op" type="hidden" value="del"/>
-                ID <?php echo $decl; ?> do usunięcia: <input type="number" name="id" value=0/>
-                <input type="submit" value="Usuń"/>
             </form>
+            <form name="edit" id="edit" method="POST" action="operation.php">
+                <?php echo '<input name="tab" type="hidden" value="'.$table.'"/>'?>
+                <input name="op" type="hidden" value="insert"/>
+            </form>
+            <table>
+                <tr>
+                    <th>ID <?php
+                        global $decl;
+                        $decl = substr($table, 0, strlen($table)-1)."u";
+                        echo $decl;
+                    ?></th>
+                    <th>czas</th>
+                    <th>data</th>
+                    <th>kierunek</th>
+                    <th>numer rejsu</th>
+                    <th>numer samolotu</th>
+                    <th>status</th>
+                </tr>
+                <?php
+                    $conn = mysqli_connect("localhost", "root", null, "egzamin");
+                    $query = "SELECT czas, kierunek, nr_rejsu, status_lotu, id, samoloty_id, dzien FROM ".$table." WHERE czas >= ? AND czas <= ? ORDER BY czas ASC";
+                    $stmt = $conn -> prepare($query);
+                    $stmt ->bind_param("ss", $from, $to);
+                    $stmt -> execute();
+                    $result = $stmt -> get_result();
+                    while($row = mysqli_fetch_row($result)){
+                        $czas = $row[0];
+                        $kierunak = $row[1];
+                        $nr = $row[2];
+                        $status = $row[3];
+                        $id = $row[4];
+                        $samolot = $row[5];
+                        $day = $row[6];
+                        echo "<tr><td>", $id, "</td><td>", $czas, "</td><td>", $day, "</td><td>", $kierunak, "</td><td>", $nr, "</td><td>", $samolot, "</td><td>", $status, "</td></tr>";
+                    }
+                    mysqli_close($conn);
+                ?>
+                <!-- Formularz dodawania/edycji (w tej samej tabeli) -->
+                <tr>
+                    <td colspan=4>
+                        
+                        Wpisz ID <?php echo $decl; ?> do edycji, lub 0 do dodania nowego przylotu
+                        <input type="submit" value="Prześlij" form="edit"/>
+                    </td>
+                    <td colspan=3>
+                        ID <?php echo $decl; ?> do usunięcia:
+                        <input type="number" name="id" value=0 form="del"/>
+                        <input type="submit" value="Usuń"      form="del"/>
+                    </td>
+                </tr>
+                <tr>
+                    <th>ID lub wstaw</th>
+                    <th>Czas</th>
+                    <th>Data</th>
+                    <th>kierunek</th>
+                    <th>Numer rejsu</th>
+                    <th>Numer samolotu</th>
+                    <th>Status</th>
+                </tr>
+                <tr>
+                    <td><input type="number" name="id" value=0 form="edit"/></td>
+                    <td><input type="time" name="czas" value=0 form="edit"/></td>
+                    <td><input type="date" name="dzien" value=0 form="edit"/></td>
+                    <td><input type="text" name="kierunek" value=0 form="edit"/></td>
+                    <td><input type="text" name="nr_rejsu" value=0 form="edit"/></td>
+                    <td><input type="number" name="samoloty_id" value=0 form="edit"/></td>
+                    <td><input type="text" name="status_lotu" value="0" form="edit"/></td>
+                </tr>
+            </table>
+            
+            
         </section>
         <section id="foot">
             <section id="foot1">
